8c4d02362bcc2d134015fa5a5a8e7afbfdfe2471
Sakshsam092005 <sakshamkankaria8196@gmail.com>
2025-06-07 23:38:48 +0530

    started eduactor frontend

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import { dummyEducatorData } from '../../assets/assets'
-import {UserButton , useUser} from '@clerk/clerk-react'
+import React, { useContext } from 'react';
+import { assets, dummyEducatorData } from '../../assets/assets';
+import { Link } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
+import { UserButton, useUser } from '@clerk/clerk-react';
 
-const Navbar = () => {
-  return (
-    <div>
-        <h1>Edu Navbar</h1>
+const Navbar = ({ bgColor }) => {
+
+  const { isEducator } = useContext(AppContext)
+  const { user } = useUser()
+  const educatorData = dummyEducatorData
+
+  return  (
+    <div className={`flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3 ${bgColor}`}>
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : 'Developers'}</p>
+        {user ? <UserButton /> : <img className='max-w-8' src={assets.profile_img} />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
