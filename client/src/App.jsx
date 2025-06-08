7cbb64c5d944d64162daae4e62eb508f5e0f254b
Sakshsam092005 <sakshamkankaria8196@gmail.com>
2025-06-08 10:14:05 +0530

    frontend sealed finally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
 import SearchBar from './components/student/SearchBar'
+import "quill/dist/quill.snow.css";
 
 const App = () => {
 
@@ -36,7 +37,7 @@ const App = () => {
         <Route path='/loading/:path' element={<Loading />} />
 
         <Route path='/educator' element={<Educator />}>
-          <Route path='educator' element={<Dashboard />} />
+          <Route path='/educator' element={<Dashboard />} />
           <Route path='add-course' element={<AddCourse />} />
           <Route path='my-courses' element={<MyCourses />} />
           <Route path='student-enrolled' element={<StudentsEnrolled />} />
